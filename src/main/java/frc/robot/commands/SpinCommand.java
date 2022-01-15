ec6302c7106cb8b0403dc5d63e285977b501961c
matt <ames1mw@gmail.com>
2022-01-15 11:47:01 -0500

    intial commit

---
+++ src/main/java/frc/robot/commands/SpinCommand.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.RomiDrivetrain;
+
+public class SpinCommand extends CommandBase {
+    private final RomiDrivetrain drivetrain;
+
+    public SpinCommand(RomiDrivetrain drivetrain) {
+        this.drivetrain = drivetrain;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        this.drivetrain.arcadeDrive(0, 0.5);
+    }
+
+    @Override
+    public boolean isFinished() {
+        var distance = this.drivetrain.getRightDistanceInch();
+        return Math.abs(distance) >= 20;
+    }
+}
